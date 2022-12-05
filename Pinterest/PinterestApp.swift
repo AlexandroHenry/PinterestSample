5136c5c90ec9a7104106109b1ac603f5dae754b6
alaxhenry <alaxhenry0121@gmail.com>
2022-12-05 10:20:41 +0900

    Initial Commit

---
+++ Pinterest/PinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestApp.swift
+//  Pinterest
+//
+//  Created by Seungchul Ha on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct PinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
